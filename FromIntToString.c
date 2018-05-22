87678855d287406856a35eb46848ace1565383d5
Elena Ramon <ramon.elena97@gmail.com>
2018-05-22 10:04:26 +0200

    File integer conversion

---
+++ FromIntToString.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(){
+
+    int numero = 1234;
+    int val = numero;
+    int count = 0;
+
+    // conteggio cifre
+    while(val != 0){
+        val = val / 10;
+        count++;
+    }
+    char *stringa = (char*) malloc(sizeof(char) * count + 1);
+
+    int i;
+
+    for(i = 0; i < count; i++){
+        val = numero % 10;
+        numero = numero / 10;
+        switch(val){
+            case 1:
+                stringa[i] = '1';
+                break;
+            case 2:
+                stringa[i] = '2';
+                break;
+            case 3:
+                stringa[i] = '3';
+                break;
+            case 4:
+                stringa[i] = '4';
+                break;
+            case 5:
+                stringa[i] = '5';
+                break;
+            case 6:
+                stringa[i] = '6';
+                break;
+            case 7:
+                stringa[i] = '7';
+                break;
+            case 8:
+                stringa[i] = '8';
+                break;
+            case 9:
+                stringa[i] = '9';
+                break;                
+            default:
+                stringa[i] = '0';
+                break;
+        }
+    }
+    i++;
+    stringa[i] = '\0';
+    int a;
+    printf("%s\n", stringa);
+    char *conv = (char*) malloc(sizeof(char) * count + 1);
+
+    for(i = count - 1, a = 0; i >= 0; i--, a++){
+        conv[i] = stringa[a];
+    }
+    conv[count] = '\0';
+    printf("%s\n", conv);
+    return 0;
+}
